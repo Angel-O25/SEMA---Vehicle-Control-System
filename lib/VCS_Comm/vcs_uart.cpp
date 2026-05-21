2b097d7b706cf16b9334cc9c72710cc89dc5983c
Angel-O25 <ortega.angel.2505@gmail.com>
2026-05-21 17:08:58 +0800

    added translated TX for debugging

--- lib/VCS_Comm/vcs_uart.cpp
+++ lib/VCS_Comm/vcs_uart.cpp
@@ -398,4 +398,12 @@ void printHexDebug(const char* prefix, const uint8_t* data, uint8_t length) {
         Serial.print(" ");
     }
     Serial.println();
+    Serial.printf("[TX decoded] RPM:%d Steer:%d State:%d Speed:%d Rev:%d\n",
+        (int16_t)((tx_buf[4]<<8)|tx_buf[5]),   // RPM
+        (uint16_t)((tx_buf[6]<<8)|tx_buf[7]),  // Steer
+        tx_buf[8],                              // State
+        tx_buf[9],                              // Speed
+        tx_buf[10]                              // Reverse
+    );
 }
+
